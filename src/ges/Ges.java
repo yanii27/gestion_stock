6e7dafa861565b3a82561adc0dda984dab4d10fc
yanii27 <yanilacamora@gmail.com>
2018-04-13 22:41:13 +0200

    initial commit

---
+++ src/ges/Ges.java
@@ -0,0 +1,17 @@
+
+package ges;
+
+/**
+ *
+ * @author YANI
+ */
+public class Ges {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+    }
+    
+}
